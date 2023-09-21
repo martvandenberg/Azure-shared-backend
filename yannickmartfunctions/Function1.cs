77d048862d34860ad96a2c534b7b7f1e78d8755b
Mart van den Berg <berg-m-van-den@belastingdienst.nl>
2023-09-21 15:08:39 +0200

    fix autoAI

--- yannickmartfunctions/Function1.cs
+++ yannickmartfunctions/Function1.cs
@@ -32,7 +32,7 @@ namespace yannickmartfunctions
                 Connection = "ConnectionStrings:cosmosdb")]out dynamic document, ILogger log)
         {
             Persoon? persoon = JsonSerializer.Deserialize<Persoon>(myQueueItem);
-            document = new { id = Guid.NewGuid().ToString(), FirstName = persoon.FirstName, LastName = persoon.LastName, LicensePlate = persoon.LicensePlate, ImageUrl = persoon.ImageUrl, Analysis = persoon.AnalysisResult, Merk = persoon.Merk };
+            document = new { id = Guid.NewGuid().ToString(), FirstName = persoon.FirstName, LastName = persoon.LastName, LicensePlate = persoon.LicensePlate, ImageUrl = persoon.ImageUrl, Analysis = persoon.AnalysisResult, Merk = persoon.MerkLicensePlate };
             log.LogInformation($"Document: {document}");
             log.LogInformation($"Document: {document.LicensePlate}");
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
@@ -46,7 +46,7 @@ namespace yannickmartfunctions
         public string ImageUrl { get; set; }
         public string AnalysisResult { get; set; }
 
-        public string Merk { get; set; }
+        public string MerkLicensePlate { get; set; }
 
     }
 }
